9a252218e4cc1e2cc3b31dceec2d675559c104eb
BJ-Jeong <ewqss0227@gmail.com>
2022-05-25 14:34:57 +0900

    0525

--- Capstone2022/app/src/main/java/com/example/capstone2022/ContactFragment.java
+++ Capstone2022/app/src/main/java/com/example/capstone2022/ContactFragment.java
@@ -1,17 +1,27 @@
 package com.example.capstone2022;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContactFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class ContactFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -22,6 +32,12 @@ public class ContactFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    RecyclerView recyclerView;
+    ArrayList<ContactModel> arrayList = new ArrayList<ContactModel>();
+    MainAdapter adapter;
+
+
 
     public ContactFragment() {
         // Required empty public constructor
@@ -58,6 +74,78 @@ public class ContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        recyclerView = getView().findViewById(R.id.recycler_view);
+        checkPermission();
         return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_CONTACTS)
+        != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    android.Manifest.permission.READ_CONTACTS)) {
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{android.Manifest.permission.READ_CONTACTS},
+                        1);
+                getContactList();
+            }
+        }
+    }
+
+    private void getContactList() {
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+
+        String sort = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME+" ASC";
+
+        Cursor cursor = getActivity().getContentResolver().query(
+                uri, null, null, null, sort
+        );
+
+        if (cursor.getCount() > 0){
+            while (cursor.moveToNext()){
+                int id1 = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                String id = cursor.getString(id1);
+
+                int name1 = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                String name = cursor.getString(name1);
+
+                Uri uriPhone = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+                String selection = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " =?";
+
+                Cursor phoneCursor = getActivity().getContentResolver().query
+                        (uriPhone, null,selection, new String[]{id}, null);
+
+                if (phoneCursor.moveToNext()){
+                    int number1 = phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    String number = phoneCursor.getString(number1);
+                    ContactModel model = new ContactModel();
+                    model.setName(name);
+                    model.setNumber(number);
+                    arrayList.add(model);
+                    phoneCursor.close();
+                }
+
+            }
+            cursor.close();
+        }
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new MainAdapter(getActivity(), arrayList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults.length > 0 && grantResults[0]
+            == PackageManager.PERMISSION_GRANTED){
+            getContactList();;
+        }else {
+            Toast.makeText(getContext(), "Pesrmission Denied.",
+                    Toast.LENGTH_SHORT).show();
+            checkPermission();
+        }
     }
 }
