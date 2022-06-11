fb13e518e81d06f600c220c72da68fb394cb75f1
lshlove228 <78405618+lshlove228@users.noreply.github.com>
2022-06-12 03:30:34 +0900

    마이페이지- 확진판정일 설정

--- Capstone2022/app/src/main/java/com/example/capstone2022/ContactMypage.java
+++ Capstone2022/app/src/main/java/com/example/capstone2022/ContactMypage.java
@@ -4,19 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import java.util.Calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactMypage extends AppCompatActivity {
 
-    ImageView back_c;
-
-    @Override    protected void onCreate(Bundle savedInstanceState) {
+    ImageView edit, back_c;
+    TextView text_con5;
+    DatePickerDialog datePickerDialog;
+    @Override    
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_con);
-
         back_c = findViewById(R.id.back_con);
-
         back_c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,7 +27,24 @@ public class ContactMypage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int pYear = calendar.get(Calendar.YEAR);
+                int pMonth = calendar.get(Calendar.MONTH);
+                int pDay = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(ContactMypage.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        month = month+1;
+                        String date = year + "/" + month + "/" + day;
+                        text_con5.setText(date);
+                    }
+                }, pYear, pMonth, pDay);
+                datePickerDialog.show();
+            }
+        });
 
 
     }
