c403eeb0fdd7478231c38f49b34d14271cbc83f9
gimhyewon <gpdnjs07092gmail.com>
2022-06-12 20:51:38 +0900

    도움말 뒤로가기 버튼이 실행이 안되는거 오류 수정

--- Capstone2022/app/src/main/java/com/example/capstone2022/HelpHome.java
+++ Capstone2022/app/src/main/java/com/example/capstone2022/HelpHome.java
@@ -1,12 +1,29 @@
 package com.example.capstone2022;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelpHome extends AppCompatActivity {
+    private String TAG = HelpHome.class.getSimpleName();
+    ImageView back_h;
+
     @Override    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_help);
+
+        back_h = findViewById(R.id.back_help);
+
+
+        back_h.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MyTntent = new Intent(HelpHome.this, MainActivity.class);
+                startActivity(MyTntent);
+            }
+        });
     }
 }
